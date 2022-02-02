4bed0fc9a84e8df7ff11818670b507de20b1f07c
Ciangciangg <paVillarosa@mcm.edu.ph>
2022-02-03 05:40:56 +0800

    Linked compiled websites and added folder images

---
+++ PatriciaProjects/Module2_Project3/script.js
@@ -0,0 +1,75 @@
+/** @format */
+
+/* Functions for animal selections */
+
+$($("<div><h1>Liked Animals/Pets</h1></dv>")).prependTo($("#like"));
+function insert() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image cat-added"></div><div class="grid"><div class="name-added"><h2>Charlotte</h2><span>2</span></div></div><div></div><div> '
+		)
+	).prependTo($("#cat"));
+}
+function insertDog() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image dog-added"></div><div class="grid"><div class="name-added"><h2>Joseph </h2><span>4</span></div></div></div> '
+		)
+	).prependTo($("#dog"));
+}
+function insertRabbit() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image rabbit-added"></div><div class="grid"><div class="name-added"><h2>Kennie and Bunnie </h2><span>1 and 3</span></div></div></div> '
+		)
+	).prependTo($("#rabbit"));
+}
+function insertBird() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image bird-added"></div><div class="grid"><div class="name-added"><h2>Felix</h2><span>5mon</span></div></div></div> '
+		)
+	).prependTo($("#bird"));
+}
+function insertIguana() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image iguana-added"></div><div class="grid"><div class="name-added"><h2>Gord </h2><span>5</span></div></div></div> '
+		)
+	).prependTo($("#iguana"));
+}
+function insertMice() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image mice-added"></div><div class="grid"><div class="name-added"><h2>Timmy</h2><span>1</span></div></div></div> '
+		)
+	).prependTo($("#mice"));
+}
+function insertDove() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image dove-added"></div><div class="grid"><div class="name-added"><h2>Dizzi</h2><span>2</span></div></div></div> '
+		)
+	).prependTo($("#dove"));
+}
+function insertEagle() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image eagle-added"></div><div class="grid"><div class="name-added"><h2>Jack</h2><span>4</span></div></div></div> '
+		)
+	).prependTo($("#eagle"));
+}
+function insertMonkey() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image monkey-added"></div><div class="grid"><div class="name-added"><h2>Abu and Abe</h2><span>2 and 5</span></div></div></div> '
+		)
+	).prependTo($("#monkey"));
+}
+function insertDuck() {
+	$(
+		$(
+			'<div class="swiper-slide"><div class="card-image duck-added"></div><div class="grid"><div class="name-added"><h2>Dolly</h2><span>2</span></div></div></div> '
+		)
+	).prependTo($("#duck"));
+}
